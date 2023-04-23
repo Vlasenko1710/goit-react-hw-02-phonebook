2b3f57fdd99494758fe6a9e58aad136b10028747
Vlasenko1710 <vlasenko_ea@ukr.net>
2023-04-23 19:54:58 +0300

    add_style

--- src/index.js
+++ src/index.js
@@ -2,10 +2,8 @@ import { App } from 'components/App/App';
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 
-
-
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App/>
+    <App />
   </React.StrictMode>
 );
